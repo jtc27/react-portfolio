88d8afe5e55afd6f09d0e8e1c0909939f749baad
Jacob Cho <jacobcho@live.com>
2023-04-27 20:34:59 -0600

    upload

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,65 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from 'react-icons/ai'
+import Image from 'next/image'
+import profile from '../public/profile.jpg'
 
 export default function Home() {
   return (
+  <div>
+    <Head>
+      <title>Jacob Cho Portfolio</title>
+      <meta />
+      <link rel="stylesheet" href="" />
+    </Head>
 
-    <div>
-      <Head>
-        <title>Jacob Cho Portfolio</title>
-        <meta />
-        <link rel="stylesheet" href="" />
-      </Head>
+    <main className='bg-white px-10'>
 
-      <main>
+      {/* NAV */}
+      <section className='bg-white min-h-screen'>
+        <nav className='py-10 mb-12 flex justify-between'>
+          <h1 className='text-xl font-burtons'>welcome</h1>
+          <ul className='flex items-center'>
+            <li>
+              <BsFillMoonStarsFill className='cursor-pointer text-xl'/>
+            </li>
+            <li>
+              <a 
+              href className='
+              text-white px-4 py-2 rounded-md ml-8
+              bg-gradient-to-r from-cyan-500 to-teal-400'>
+                resume
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-      </main>
-    </div>
+      <div className='text-center p-5'>
+        <h2 className='text-2xl text-teal-700 font-medium'>Jacob Cho</h2>
+        <h3 className='text-xl py-2'>Developer</h3>
+        <p className='text-md py-5 leading-8 text-gray-800'>
+        Hi! I'm Jacob,
+        Software Developer
+        </p>
+      </div>
 
+      <div className='text-5xl flex justify-center gap-16 py-3 text-gray-500'>
+        <AiFillTwitterCircle/>
+        <AiFillLinkedin />
+        <AiFillYoutube />
+      </div>
 
+      <div className='relative bg-gradient-to-b from-teal-500 rounded-full w-40 h-40 mt-20 mx-auto'>
+        <Image src={profile}/>
+      </div>
+
+      </section>
+
+    </main>
+  </div>
   )
 }
